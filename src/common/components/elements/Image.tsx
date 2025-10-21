748a6da97a9bd275d24d7fc58c58fdeb47c01f42
pong7772 <visothipong7772@gmail.com>
2025-10-21 14:35:00 +0200

    fix(image): properly handle fill prop for blog and story card display

--- src/common/components/elements/Image.tsx
+++ src/common/components/elements/Image.tsx
@@ -9,10 +9,11 @@ type ImageProps = {
   rounded?: string;
   width?: number | null;
   height?: number | null;
+  fill?: boolean;
 } & NextImageProps;
 
 const Image = (props: ImageProps) => {
-  const { alt, src, className, rounded, width, height, ...rest } = props;
+  const { alt, src, className, rounded, width, height, fill, ...rest } = props;
 
   return (
     <div className={clsx('next-image-wrapper overflow-hidden', rounded)}>
@@ -24,8 +25,9 @@ const Image = (props: ImageProps) => {
         quality={100}
         placeholder='blur'
         blurDataURL='data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAABAAEDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/8QAFQEBAQAAAAAAAAAAAAAAAAAAAAX/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwCdABmX/9k='
-        width={width || undefined}
-        height={height || undefined}
+        width={fill ? undefined : width || undefined}
+        height={fill ? undefined : height || undefined}
+        fill={fill}
         unoptimized={false}
         {...rest}
       />
